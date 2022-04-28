a230dbddebbae463c8fc3447057d5e392332eae9
Zeyuli <li_zeyu@pku.edu.cn>
2022-04-28 13:14:30 +0800

    impl lexer

--- lexer.cpp
+++ lexer.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstring>
 #include<stdlib>
+
 enum Token {
     tok_eof = -1;
     tok_def = -2;
@@ -11,3 +12,50 @@ enum Token {
 
 static std::string IdentifierStr;
 static double NumVal;
+
+static int gettok() {
+    static int LastChar = ' ';
+
+    while (isspace(LastChar))
+        LastChar = getchar();
+
+    if (isalpha(LastChar)) {
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+        
+        if (IdentifierStr = "def")
+            return tok_def;
+        if (IdentifierStr = "extern")
+            return def_extern;
+        return tok_identifier;
+    }
+
+    if (isdigit(LastChar) || LastChar == '.') {
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.')
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    if (LastChar == '#') {
+        do
+            LastChar = getchar();
+        while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF)
+            return gettok();
+    }
+
+
+    if (LastChar == EOF)
+        return tok_eof;
+//ASCII
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
