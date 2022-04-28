81de59e4cecd89cb44501f06204f07e2a18e149d
Zeyuli <li_zeyu@pku.edu.cn>
2022-04-28 17:28:22 +0800

    impl AST class

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,16 +1,65 @@
 #ifndef __KALEIDOSCOPE_INCLUDE_ASC_HPP__
 #define __KALEIDOSCOPE_INCLUDE_ASC_HPP__
 #include "lexer.hpp"
+#include <vector>
+#include <memory>
 
 class ExprAST {
 public:
-    virtual ~ExprAST() {}
+    virtual ~ExprAST(); 
 }
 
 class NumberExprAST : public ExprAST {
     double Val;
 
 public:
-    NumberExprAST(double Val) : Val(Val) {}
+    NumberExprAST(double Val);
 };
+
+// Expression class for a variable
+class VariableExprAST : public ExprAST {
+    std::string Name;
+
+public: 
+    VariableExprAST(const std::string &Name); 
+};
+
+// Expression class for a  binary operator
+class BinaryExprAST : public ExprAST {
+    char op;
+    std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+    BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS,
+                    std::unique_ptr<ExprAST> RHS);
+}
+
+// Expression class for a function call
+class CallExprAST : public ExprAST {
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+    CallExprAST(const std::string &Callee,
+                std::vector<std::unique_ptr<ExprAST>> Args);
+};
+
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+
+public:
+    PrototypeAST(const std::string &name, std::vector<std::string> Args);
+    const std::string &getName(); 
+};
+
+class FunctionAST {
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+                std::unique_ptr<ExprAST> Body);
+};
+
 #endif //__KALEIDOSCOPE_INCLUDE_AST_HPP__
--- src/ast.cpp
+++ src/ast.cpp
@@ -1,2 +1,17 @@
 #include "ast.hpp"
 
+ExprAST::~ExprAST(){}
+NumberExprAST::NumberExprAST(double Val) : Val(Val) {}
+VariableExprAST::VariableExprAST(const std::string &Name) : Name(Name) {}
+BinaryExprAST::BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS, 
+                             std::unique_ptr<ExprAST> RHS)
+                             : Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+CallExprAST::CallExprAST(const std::string &Callee,
+                         std::vector<std::unique_ptr<ExprAST>> Args)
+                         : Callee(Callee), Args(std::move(Args)) {}
+PrototypeAST::PrototypeAST(const std::string &name, std::vector<std::string> Args
+                           : Name(name), Args(std::move(Args)) {}
+PrototypeAST::const string &getName() { return Name; }
+FunctionAST::FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+                         std::unique_ptr<ExprAST> Body)
+                         : Proto(std::move(Proto)), Body(std:move(Body)) {}
